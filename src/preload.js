b21c8f71c19c07addff159be079d0df52b41b26b
Chandra Sekhar Chaganti <cc@sumvid.ai>
2026-04-26 16:33:38 -0500

    Create src/preload.js - Electron IPC bridge with contextBridge

---
+++ src/preload.js
@@ -0,0 +1,26 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, data) => {
+    const validChannels = [
+      'render-page', 'syllable-update', 'animation-reset',
+      'countdown', 'display-mode', 'spm-change',
+      'show-instruction', 'dismiss-instruction',
+      'open-projector', 'close-projector'
+    ];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  on: (channel, callback) => {
+    const validChannels = [
+      'render-page', 'syllable-update', 'animation-reset',
+      'countdown', 'display-mode', 'spm-change',
+      'show-instruction', 'dismiss-instruction',
+      'projector-status'
+    ];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => callback(...args));
+    }
+  }
+});
